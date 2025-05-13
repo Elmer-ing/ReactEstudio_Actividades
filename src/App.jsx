d45b700e195dbd07dff1cd7cb35071619956dfdb
Elmer-ing <emosquerada@cesde.net>
2025-05-12 21:49:23 -0500

    Actualizacion de App

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
-import FormularioContacto from './components/act-hooks-11-mayo/FormularioContacto';
-// import Clima from './components/act-hooks-11-mayo/Clima';
-// import TMDBComponent from './components/10-mayo/TMDBComponent'
-// import './App.css'
+import Home from './components/Paginas/Home';
+import './App.css'
 
 function App() {
   return (
     <>
-      <FormularioContacto />
+      <Home />
     </>
   )
 }
